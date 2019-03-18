a31238080b1f6f13f80fd8b7e05f5c3094ad9b3b
gplatono <gr.plat@gmail.com>
2019-03-18 17:35:17 -0400

    edited parser

--- ulf_parser.py
+++ ulf_parser.py
@@ -7,16 +7,18 @@ class TreeNode(object):
         self.children = children
 
     def __str__(self):
-        output = ""
+        if self.content is None and self.__name__ != "NArg":
+            return self.__name__
+        output = self.__name__ + ": "
         if type(self.content) == str:
             output += self.content
         elif self.content is not None:
             output += self.content.__str__()
 
         return output
-        return (self.content if isinstance(self.content, str) \
-                else (self.content.__str__() if self.content is not None else "")) \
-                     + (" [" + " ".join([child.__str__() for child in self.children if child is not None]) + "]" if self.children is not None else "")
+        #return (self.content if isinstance(self.content, str) \
+        #        else (self.content.__str__() if self.content is not None else "")) \
+        #             + (" [" + " ".join([child.__str__() for child in self.children if child is not None]) + "]" if self.children is not None else "")
 
     def get_type(self):
         return str(type(self)).split("'")[1].split(".")[1]
@@ -46,38 +48,78 @@ grammar['halfway.adv-a'] = lambda x: TAdv(x)
 grammar['slightly.adv-a'] = lambda x: TAdv(x)
 grammar['directly.adv-a'] = lambda x: TAdv(x)
 
-grammar['block.n'] = lambda x: TNoun(x)
-grammar['{block}.n'] = lambda x: TNoun(x)
+#grammar['block.n'] = lambda x: TNoun(x)
+#grammar['{block}.n'] = lambda x: TNoun(x)
+grammar['block.n'] = lambda x: NArg(obj_type = x)
+grammar['{block}.n'] = lambda x: NArg(obj_type = x)
+grammar['table.n'] = lambda x: NArg(obj_type = x, obj_id = "TABLE")
+grammar['stack.n'] = lambda x: NArg(obj_type = x, obj_id = "STACK")
+grammar['row.n'] = lambda x: NArg(obj_type = x, obj_id = "STACK")
+grammar['thing.n'] = lambda x: NArg(obj_type = None, obj_id = None)
+grammar['{thing}.n'] = lambda x: NArg(obj_type = None, obj_id = None)
+
 
 grammar['red.a'] = lambda x: TAdj(x)
 grammar['green.a'] = lambda x: TAdj(x)
 grammar['blue.a'] = lambda x: TAdj(x)
 
-grammar['plur'] = lambda x: TPlurMarker(x)
-grammar['pres'] = lambda x: TTenseMarker(x)
-grammar['be.v'] = lambda x: TCopulaBe(x)
+grammar['left.a'] = lambda x: TAdj(x)
+grammar['right.a'] = lambda x: TAdj(x)
+grammar['top.a'] = lambda x: TAdj(x)
+grammar['high.a'] = lambda x: TAdj(x)
+grammar['low.a'] = lambda x: TAdj(x)
+grammar['last.a'] = lambda x: TAdj(x)
+grammar['first.a'] = lambda x: TAdj(x)
 
-grammar['nquan'] = lambda x: TQuanMarker(x)
-grammar['fquan'] = lambda x: TQuanMarker(x)
-
-grammar['sub'] = lambda x: TSubMarker(x)
+grammar['plur'] = lambda x: TPlurMarker()
+grammar['pres'] = lambda x: TTenseMarker()
+grammar['be.v'] = lambda x: TCopulaBe()
+grammar['nquan'] = lambda x: TQuanMarker()
+grammar['fquan'] = lambda x: TQuanMarker()
+grammar['most-n'] = lambda x: TSuperMarker()
+grammar['most'] = lambda x: TSuperMarker()
+grammar['sub'] = lambda x: TSubMarker()
+grammar['?'] = lambda x: TQMarker()
+grammar['n+preds'] = lambda x: TNModMarker()
 
 grammar['which.d'] = lambda x: TDet(x)
 grammar['the.d'] = lambda x: TDet(x)
 grammar['a.d'] = lambda x: TDet(x)
 grammar['other.d'] = lambda x: TDet(x)
 
+grammar['|Nvidia|'] = lambda x: TName(x)
+grammar['|Toyota|'] = lambda x: TName(x)
+grammar['|McDonalds|'] = lambda x: TName(x)
+grammar['|SRI|'] = lambda x: TName(x)
+grammar['|Starbucks|'] = lambda x: TName(x)
+grammar['|Texaco|'] = lambda x: TName(x)
 
-grammar[("TTenseMarker", "TCopulaBe")] = lambda x, y: NVerbHead(TCopulaBe, TTenseMarker)
+grammar['|nvidia|'] = lambda x: TName(x)
+grammar['|toyota|'] = lambda x: TName(x)
+grammar['|mcdonalds|'] = lambda x: TName(x)
+grammar['|sri|'] = lambda x: TName(x)
+grammar['|starbucks|'] = lambda x: TName(x)
+grammar['|texaco|'] = lambda x: TName(x)
 
-grammar[("TAdj", "TNoun")] = lambda x, y: NArg(name = y.content, mods = [x])
-grammar[("TAdj", "NArg")] = lambda x, y: NArg(name = y.content, mods = y.mods + [x], det = y.det, plur = y.plur)
 
-grammar[("TName", "TNoun")] = lambda x, y: NArg(name = x.content + " " + y.content)
-grammar[("TPlur", "TNoun")] = lambda x, y: NArg(name = y.content, plur = True)
 
-grammar[("TDet", "NArg")] = lambda x, y: NArg(name = y.content, mods = y.mods, det = x, plur = y.plur)
-grammar[("TDet", "TNoun")] = lambda x, y: NArg(name = y.content, det = x)
+
+
+#grammar[("TTenseMarker", "TCopulaBe")] = lambda x, y: NVerbHead(TCopulaBe, TTenseMarker)
+grammar[("TSuperMarker", "TAdj")] = lambda x, y: TAdj(content = y.content, mod = x)
+
+grammar[("TName", "NArg")] = lambda x, y: NArg(obj_type = y.obj_type, obj_id = x.content)
+grammar[("TDet", "NArg")] = lambda x, y: NArg(obj_type = y.obj_type, obj_id = y.obj_id, mods = y.mods, det = x, plur = y.plur)
+grammar[("TAdj", "NArg")] = lambda x, y: NArg(obj_type = y.obj_type, obj_id = y.obj_id, mods = y.mods + [x], det = y.det, plur = y.plur)
+grammar[("TPlurMarker", "NArg")] = lambda x, y: NArg(obj_type = y.obj_type, obj_id = y.obj_id, mods = y.mods + [x], det = y.det, plur = True)
+
+
+grammar[("TAdj", "TNoun")] = lambda x, y: NArg(obj_type = y.content, mods = [x])
+
+grammar[("TName", "TNoun")] = lambda x, y: NArg(obj_type = x.content + " " + y.content)
+grammar[("TPlur", "TNoun")] = lambda x, y: NArg(obj_type = y.content, plur = True)
+
+grammar[("TDet", "TNoun")] = lambda x, y: NArg(obj_type = y.content, det = x)
 grammar[("NArg", "TPrep", "NArg")] = lambda x, y, z: NRel(y.content, [x, z])
 grammar[("TQ", "NRel")] = lambda x, y: NYesNo(y)
 
@@ -98,28 +140,48 @@ class TCopulaBe(TreeNode):
     __name__ = "TCopulaBe"
     def __init__(self, content=None):
         super(TCopulaBe, self).__init__(content, None)
+
+    #def __str__(self):
+    #    return "BE"
     
 class TPlurMarker(TreeNode):
     __name__ = "TPlurMarker"
-    def __init__(self, content=None):
-        super(TPlurMarker, self).__init__(content, None)
+    def __init__(self):
+        super(TPlurMarker, self).__init__(None, None)
 
 class TQuanMarker(TreeNode):
     __name__ = "TQuanMarker"
+    def __init__(self):
+        super(TQuanMarker, self).__init__(None, None)
+
+class TSuperMarker(TreeNode):
+    __name__ = "TSuperMarker"
     def __init__(self, content=None):
-        super(TQuanMarker, self).__init__(content, None)
+        super(TSuperMarker, self).__init__(None, None)
 
 class TSubMarker(TreeNode):
     __name__ = "TSubMarker"
-    def __init__(self, content=None):
-        super(TQuanMarker, self).__init__(content, None)
-
+    def __init__(self):
+        super().__init__(content, None)
 
 class TTenseMarker(TreeNode):
     __name__ = "TTenseMarker"
     def __init__(self, content=None):
-        super(TTenseMarker, self).__init__(content, None)
+        super().__init__(content, None)
 
+class TQMarker(TreeNode):
+    __name__ = "TQMarker"
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "?"
+
+class TNModMarker(TreeNode):
+    __name__ = "TNModMarker"
+    def __init__(self, content=None):
+        super(TNModMarker, self).__init__()
+      
 class TDet(TreeNode):
     __name__ = "TDet"
     def __init__(self, content=None):
@@ -147,8 +209,9 @@ class TPro(TreeNode):
 
 class TAdj(TreeNode):
     __name__ = "TAdj"
-    def __init__(self, content=None):
+    def __init__(self, content=None, mod=None):
         super(TAdj, self).__init__(content, None)
+        self.mod = mod
 
 class TAdv(TreeNode):
     __name__ = "TAdv"
@@ -165,14 +228,6 @@ class TUnknown(TreeNode):
     def __init__(self, content=None):
         super(TUnknown, self).__init__(content, None)
 
-class TQ(TreeNode):
-    def __init__(self):
-        pass
-
-    def printable(self):
-        return "?"
-
-
 class NColl(TreeNode):
     def __init__(self, children):
         super(NColl, self).__init__(None, children)
@@ -183,11 +238,17 @@ class NYesNo(TreeNode):
 
 class NArg(TreeNode):
     __name__ = "NArg"
-    def __init__(self, name=None, mods=[], det=None, plur=False):
-        self.content = name
+    def __init__(self, obj_type=None, obj_id=None, mods=[], det=None, plur=False):
+        self.obj_type = obj_type
+        self.obj_id = obj_id
         self.mods = mods
         self.det = det
         self.plur = plur
+
+    def __str__(self):
+        return self.obj_type+":\n" + self.obj_id.__str__() + "\n"+ self.mods.__str__() + "\n" +\
+            self.det.__str__() + "\n" + self.plur.__str__()
+    
 
     def printable(self):
         return (self.content if isinstance(self.content, str) \
@@ -199,7 +260,9 @@ class NArg(TreeNode):
        
 class ULFQuery(object):
     def __init__(self, input):
-        self.query_tree = self.parse_tree(self.lispify(input))
+        self.lispified = self.lispify(input)
+        self.preprocessed = self.process_sub(self.lispified)
+        self.query_tree = self.parse_tree(self.preprocessed)
 
     def terminal_node(self, token):
         if token in relations:
@@ -220,29 +283,38 @@ class ULFQuery(object):
             return TPlur()
 
     def parse_tree(self, tree):
+        print ("INIT: ", tree)
         if type(tree) == str:
             if tree in grammar:
-                print (tree, grammar[tree])
+                #print (tree, grammar[tree])
+                print ("PROC: ", tree)
                 return grammar[tree](tree)
             else:
+                print ("PROC: ", tree)
                 return TUnknown(tree)
 
         tree = [self.parse_tree(node) for node in tree]
+
+        if type(tree[0]) == TNModMarker:
+            tree[1].mods += tree[2:]
+            return tree[1]
 
         while len(tree) >= 2 and (tree[0].__name__, tree[1].__name__) in grammar:
             substitute = grammar[(tree[0].__name__, tree[1].__name__)](tree[0], tree[1])
             #print("result: ", substitute)
             tree[0] = substitute
-                       
+
+        print ("PROC: ", tree)
         return tree[0] #if len(tree) == 1 else tree
 
-    def move_sub(self, tree, expr=None):
-        if expt == None:
+    def process_sub(self, tree, expr=None):
+        if type(tree) == list:
             if tree[0] == 'sub':
-                expr = tree[0]
-                return move_sub(tree[1], expr)
-            else: 
-            
+                return self.process_sub(tree[2], tree[1])
+            else:
+                return [self.process_sub(branch, expr) for branch in tree]
+        else:
+            return expr if (tree == "*h" and expr is not None) else tree
 
         '''       
         if type(tree) != list:
@@ -340,13 +412,15 @@ str = "((coll-of |B1| |B2| |B3|) (semval (what.d shape-pred.n)))"
 query = ULFQuery(str)
 print (query.query)
 '''
-
 f = open("sqa_input.bw")
 for ulf in f.readlines():
+    ulf = ulf.lower().strip()
     if ";;" not in ulf and ulf != "":
-        ulf = ulf.lower()
+        print (ulf)
         query = ULFQuery(ulf)
+        print (query.preprocessed)
         print (query.query_tree)
+        #break
 
 #det = Det("test", ["children"])
 #print(det.printable())
